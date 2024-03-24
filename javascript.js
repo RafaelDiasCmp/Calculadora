89df4f61ba104ee442540f6d23458d97b2085833
Rafael Dias <raffaeldiascampos@gmail.com>
2024-03-23 22:27:32 -0300

    closes #4 Implementar as 4 operações matemáticas

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
-function calc(){
+function calc(op){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    var output = 0;
 
-    var output = num1 + num2;
+    switch(op){
+        case '+' : output = num1 + num2; break;
+        case '-' : output = num1 - num2; break;
+        case '*' : output = num1 * num2; break;
+        case '/' : output - num1 / num2; break;
+    }
 
     document.getElementById("output").value = output;
-
 }
